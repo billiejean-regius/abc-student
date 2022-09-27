355d72042709e09647ac41d800492022461f640b
phiddipus <bjb462@nyu.edu>
2022-09-27 08:29:57 +0800

    A lot of things...

---
+++ mini-projects/01/css/assets/script.js
@@ -0,0 +1,38 @@
+const cards = document.querySelectorAll(".flip_card");
+
+    function flipCard() {
+    this.classList.toggle("flip");
+    }
+    cards.forEach((card) => card.addEventListener("click", flipCard));
+
+// lets make a random image generator for our cards
+// let random_card = new Array();  
+    // random_card[0] = new Image();
+    // random_card[0].src = 'tarot_01.png';
+
+    // random_card[1] = new Image();
+    // random_card[1].src = 'tarot_02.png';
+
+    // random_card[2] = new Image();
+    // random_card[2].src = 'tarot_03.png';
+
+let tarot01 = document.createElement("img");
+tarot01.src = 'tarot_01.png';
+let tarot02 = document.createElement("img");
+tarot02.src = 'tarot_01.png';
+let tarot03 = document.createElement("img");
+tarot03.src = 'tarot_01.png';
+
+let random_card = [
+    tarot01,
+    tarot02,
+    tarot03,
+]
+
+const getRandom = (random_card) => {
+    return random_card[Math.floor(Math.random() * random_card.length)];
+};
+
+const card_image = document.querySelectorAll(".front");
+
+card_image.style.background-image("url"(getRandom(random_card)));
--- mini-projects/01/js/script.js
+++ mini-projects/01/js/script.js
@@ -1,6 +1,61 @@
-const cards = document.querySelectorAll(".flip_card");
+const inputField = document.getElementById('inputField');
+let button = document.getElementById("button");
+let enough = document.getElementById("enough");
 
-function flipCard() {
-  this.classList.toggle("flip");
-}
-cards.forEach((card) => card.addEventListener("click", flipCard));
+
+clickMe.addEventListener("click", () => {
+    card_deck.innerHTML = "";
+    let result = inputNum.value;
+
+    let num = Number(result);
+    console.log(num);
+
+    for (let i = 0; i < num ; i++) {
+        let card_deck = document.getElementById("card_deck");
+        let container = document.createElement("div");
+        let flip_card = document.createElement("div");
+        let front_side = document.createElement("div");
+        let back_side = document.createElement("div");
+        let text_cont = document.createElement("div");
+        let text_wrap = document.createElement("span");
+        let text_span = document.createElement("span");
+        
+            card_deck.appendChild(container); // A
+            container.append(flip_card, text_cont); // A => BC
+                container.classList.add("container")
+            flip_card.append(front_side, back_side); // B => DE
+                flip_card.classList.add("flip_card");
+                    front_side.classList.add("front", "side"); 
+                    back_side.classList.add("back", "side");
+            text_cont.appendChild(text_wrap); // C => F
+            text_wrap.appendChild(text_span); // F => G
+                text_cont.classList.add("text_container");
+                text_wrap.classList.add("text_wrapper");
+                text_span.classList.add("letters");
+
+            text_span.innerHTML = "Hello!";    
+
+    }
+    const cards = document.querySelectorAll(".flip_card");
+
+    function flipCard() {
+    this.classList.toggle("flip");
+    }
+    cards.forEach((card) => card.addEventListener("click", flipCard));
+
+});
+
+let sw = screen.width;
+let sh = screen.height;
+
+// random card window
+function randCard(){
+    // generating a random y and x location for the card
+    let ranX = Math.random()*sh - 200;
+    let ranY = Math.random()*sh - 200;
+
+    let win = window.open("css/assets", "_blank", "width=215, height=300, left="+ranX+", top="+ranY);
+  
+}; 
+
+button.addEventListener("click", randCard);
