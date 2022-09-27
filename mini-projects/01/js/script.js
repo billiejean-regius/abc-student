6e2d14263dc58c312ecbce26c6b61a98f4b5c7ab
phiddipus <bjb462@nyu.edu>
2022-09-27 14:20:19 +0800

    Three card.

--- mini-projects/01/js/script.js
+++ mini-projects/01/js/script.js
@@ -53,10 +53,13 @@ let sh = screen.height;
 // random card window
 function randCard(){
     // generating a random y and x location for the card
-    let ranX = Math.random()*sh - 200;
-    let ranY = Math.random()*sh - 200;
+    // let ranX = Math.random()*sh - 200;
+    // let ranY = Math.random()*sh - 200;
 
-    let win = window.open("css/assets", "_blank", "width=215, height=300, left="+ranX+", top="+ranY);
+    // let win = window.open("css/assets", "_blank", "width=215, height=300, left="+ranX+", top="+ranY);
+    let win1 = window.open("css/assets", "_blank", "width=215, height=300, left="+200+", top="+100);
+    let win2 = window.open("css/assets", "_blank", "width=215, height=300, left="+400+", top="+100);
+    let win3 = window.open("css/assets", "_blank", "width=215, height=300, left="+600+", top="+100);
   
 }; 
 
