a5dffe1efa6b33246e312a8a7f5b2af67fa3911e
phiddipus <bjb462@nyu.edu>
2022-10-06 10:02:33 +0800

    commit

--- circus/mini-projects/03/js/script.js
+++ circus/mini-projects/03/js/script.js
@@ -1,3 +1,42 @@
+let on = document.getElementById('on');
+let off = document.getElementById('off');
+
+// create a speaker 
+let context = new AudioContext();
+
+let destination = context.destination;
+
+
+// create oscillator
+let oscillator = context.createOscillator();
+oscillator.frequency.value = 440; //Hertz, 440 correaltes to an A note (4th octave)
+
+
+//create volume knob
+let gain = context.createGain();
+
+
+// plug oscillator into the volume knob 
+oscillator.connect(gain);
+
+
+// plug the volume knob into the speaker
+gain.connect(destination);
+
+// play sound
+// use buttons to play sound
+let oscillatorHasStarted = false;
+on.addEventListener("click", ()=>{
+    if(oscillatorHasStarted == false){
+        oscillator.start(0);
+        oscillatorHasStarted = true;
+    }
+    gain.gain.value = 1;
+});
+
+off.addEventListener("click", ()=>{
+    gain.gain.value = 0;
+})
 
           
         
