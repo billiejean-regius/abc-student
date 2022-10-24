ffd1410c6edcf6421b0acaa0ebda128374bd017a
phiddipus <bjb462@nyu.edu>
2022-10-24 19:00:24 +0800

    Commit

--- circus/mini-projects/04/popup/css/assets/script.js
+++ circus/mini-projects/04/popup/css/assets/script.js
@@ -1,7 +1,14 @@
 let swat = document.getElementById('swatBug');
 
 function swatBug(){
-    
+    let chance = Math.random() * 100
+
+    if (chance >= 0 && chance < 50) {
+        alert("hi");
+      }
+      else if (chance >= 50 && chance <= 100) {
+        alert("bye");
+      }
     //if coackroach create two
     //% chance of kill
 }; 
--- circus/mini-projects/04/popup/js/popup.js
+++ circus/mini-projects/04/popup/js/popup.js
@@ -32,6 +32,8 @@
 
 let bugMessage = document.getElementById("bugged");
 let calcBtn = document.getElementById("calcButton");
+let answer = document.getElementById("answer");
+let equal = document.getElementById('equal');
 
 let count = 0; 
 
@@ -39,17 +41,42 @@ let count = 0;
 //   count++;
 //   display.innerHTML = count;
 // });
+let inputBtns = document.getElementsByClassName('inputValueButton');
+console.log(inputBtns);
 
 
+
+
+
+document.addEventListener('click', function handleClick(event) {
+    thisValue = event.target.value;
+    console.log("background script: GOT VALUE:", thisValue) 
+  });
+
+  //get calculation value => generate number of bugs?
+  function preloadImage(url)
+  {
+      var img = new Image();
+      img.src = "/test/example.jpg";
+  }
+  
 let sw = screen.width;
 let sh = screen.height;
 
-function getBugged(){
-    let ranX = Math.random()*sh;
-    let ranY = Math.random()*sh;
-
-    let win1 = window.open("https://127.0.0.1:5500/abc-student/circus/mini-projects/04/popup/css/assets/index.html", "_blank", "width=500, height=500, left="+ranX+", top="+ranY);
-  
+  function getBugged(){
+    for (let i = 0; i < thisValue ; i++) {
+        (function(i) {
+            setTimeout(function() {
+                let ranX = Math.random()*sh;
+                let ranY = Math.random()*sh;
+                // let win1 = window.open("https://127.0.0.1:5500/abc-student/circus/mini-projects/04/popup/css/assets/index.html", "_blank", "width=500, height=500, left="+ranX+", top="+ranY);
+        
+            }, 3000 * i);
+       
+        })(i);
+        
+    }
 }; 
 
-calcBtn.addEventListener("click", getBugged);
+
+equal.addEventListener("click", getBugged);
