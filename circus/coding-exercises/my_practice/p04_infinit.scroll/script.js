310bd5f83d04c328e443536fe6fb40e03222bdea
phiddiRegius <bjb462@nyu.edu>
2022-11-18 23:51:28 +0800

    Infinite Hrzn & Vert Scroll

--- circus/coding-exercises/my_practice/p04_infinit.scroll/script.js
+++ circus/coding-exercises/my_practice/p04_infinit.scroll/script.js
@@ -20,8 +20,12 @@ webPage02.setAttribute('id', 'webPage02');
 let webPage03 = document.createElement('div');
 webPage03.setAttribute('id', 'webPage03');
 
+let webPage = [
+    webPage01,
+    webPage02,
+    webPage03,
+];
 
-webPages.append(webPage01, webPage02, webPage03);
 document.body.append(scrollVert, scrollHrzn);
 
 const getOffset = (el) => {
@@ -53,6 +57,7 @@ Element.prototype.appendAfter = function (element) {
 function scrollVertPage() {
     document.getElementsByTagName("body")[0].innerHTML = "";
     document.body.append(webPages);
+    webPages.append(webPage[0], webPage[1], webPage[2]);
 
     webPage01.innerHTML = innards;
     webPage02.innerHTML = innards;
@@ -84,39 +89,87 @@ function scrollVertPage() {
 
 function scrollHrznPage() {
     document.getElementsByTagName("body")[0].innerHTML = "";
-    document.body.append(webPages);
 
     webPage01.innerHTML = innards;
     webPage02.innerHTML = innards;
     webPage03.innerHTML = innards;
 
-    webPages.setAttribute("id", "parentPage");
-    webPage01.setAttribute("class", "childPages");
-    webPage02.setAttribute("class", "childPages");
-    webPage03.setAttribute("class", "childPages");
+    let sliderWrap = document.createElement('div');
+        sliderWrap.classList.add('sliderWrap');
+    let slider = document.createElement('div');
+        slider.classList.add('slider');
+    let clonesWidth;
+    let sliderWidth;
+    let clones =[];
+    let disableScroll = false;
+    let scrollPos;
 
-    console.log("horizontal scroll");
+    let items = [];
+    // console.log(webPage)
 
-    let intervalCount = 3;
-    setInterval(()=>{
-        window.scrollBy(intervalCount, 0);
-        // console.log("This is", intervalCount);
-        
-        //the removed elements is causing all the other elements to shift left
-        if(getOffset(webPage01).right > 0) {
-            webPage01.appendAfter(webPage03);
-            console.log("Moved Page 1");
+    for(let i = 1; i < 4; i++) {
+        let sliderItem = document.createElement('div');
+        // sliderItem.setAttribute("id", "item" + i);
+        slider.append(sliderItem);
+        sliderItem.append(webPage[i - 1]);
+        webPage[i - 1].setAttribute('class', 'pages')
+        items.push(sliderItem);
+    };
+
+    sliderWrap.appendChild(slider);
+    document.body.append(sliderWrap);
+    
+    items.forEach(item => {
+        let clone = item.cloneNode(true);
+        clone.classList.add('clone');
+        slider.append(clone);
+        clones.push(clone);
+    });
+
+    function getClonesWidth() {
+        let width = 0; 
+        clones.forEach(clone => {
+            width += clone.offsetWidth;
+        })
+        return width;
+    }
+
+    function getScrollPos() {
+        return window.scrollY;
+    }
+
+    let intervalCount = 2;
+    
+    
+    function scrollUpdate() {
+        scrollPos = getScrollPos();
+        if(clonesWidth + scrollPos >= sliderWidth) {
+            window.scrollTo(0, intervalCount);
+            
+        } else if (scrollPos <= 0) {
+            window.scrollTo({top: sliderWidth - clonesWidth -1})
         }
-        // if(getOffset(webPage02).right < 0) {
-        //     webPage02.appendAfter(webPage01);
-        //     console.log("Moved Page 2");
-        // }
-        // if(getOffset(webPage03).right < 0) {
-        //     webPage03.appendAfter(webPage02);
-        //     console.log("Moved Page 3");
-        // }
-    }, 50);
- 
+
+        slider.style.transform = `translateX(${-window.scrollY}px)`
+        requestAnimationFrame(scrollUpdate);
+    }
+
+    function onLoad() {
+        calculateDimension();
+        document.body.style.height = `${sliderWidth}px`;
+        window.scrollTo(0, intervalCount);
+        setInterval(()=>{
+            window.scrollBy(0, intervalCount);
+        }, 50);
+        scrollUpdate();
+    }
+
+    function calculateDimension() {
+        sliderWidth = slider.getBoundingClientRect().width;
+        clonesWidth = getClonesWidth();
+    }
+
+    onLoad()
 }
 
  scrollVert.addEventListener("click", scrollVertPage)
