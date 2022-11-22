02809c92b9af5c4a2f09dfaf9c0cd46586f73453
phiddiRegius <bjb462@nyu.edu>
2022-11-22 09:10:09 +0800

    Changes and stuff

--- circus/coding-exercises/my_practice/cssAnimations/p03_keyframesCarousel/js/script.js
+++ circus/coding-exercises/my_practice/cssAnimations/p03_keyframesCarousel/js/script.js
@@ -5,6 +5,7 @@
 // let sh = screen.height;
 
 // name, link, image
+
 class Project {
   constructor(name, link, image) {
       this.name = name;
@@ -79,9 +80,14 @@ const getOffset = (el) => {
 // let linkText = document.createElement('div');
 // linkText.setAttribute('class', 'linkText');
 
+let sliderContainer = document.createElement('div');
+sliderContainer .setAttribute('class', 'swiper');
+let sliderWrapper = document.createElement('div');
+sliderWrapper .setAttribute('class', 'swiper-wrapper');
+
 for (let i = 0; i < 10 ; i++) {
   let slides = document.createElement('div');
-    slides.setAttribute('class', 'slides');
+    slides.setAttribute('class', 'swiper-slide');
 
   let carousel = document.createElement('div');
     carousel.setAttribute('class', 'carousel');
@@ -115,7 +121,9 @@ for (let i = 0; i < 10 ; i++) {
   beams.style.backgroundImage = 'url(assets/sketches/beamSketch.png)';
   seats.style.backgroundImage = 'url(assets/sketches/seatSketch.png)';
 
-    document.body.append(slides);
+    document.body.append(sliderContainer);
+    sliderContainer.append(sliderWrapper)
+    sliderWrapper.append(slides);
     slides.append(carousel, info);
     info.append(title, link);
     title.append(titleText);
@@ -134,54 +142,6 @@ if( screen.width <= 480 ) {
   // document.body.append(carousel);
   // carousel.append(beams, seats);
 }
-
-function detectswipe(el,func) {
-  swipe_det = new Object();
-  swipe_det.sX = 0; swipe_det.sY = 0; swipe_det.eX = 0; swipe_det.eY = 0;
-  var min_x = 30;  //min x swipe for horizontal swipe
-  var max_x = 30;  //max x difference for vertical swipe
-  var min_y = 50;  //min y swipe for vertical swipe
-  var max_y = 60;  //max y difference for horizontal swipe
-  var direc = "";
-  ele = document.getElementById(el);
-  ele.addEventListener('touchstart',function(e){
-    var t = e.touches[0];
-    swipe_det.sX = t.screenX; 
-    swipe_det.sY = t.screenY;
-  },false);
-  ele.addEventListener('touchmove',function(e){
-    e.preventDefault();
-    var t = e.touches[0];
-    swipe_det.eX = t.screenX; 
-    swipe_det.eY = t.screenY;    
-  },false);
-  ele.addEventListener('touchend',function(e){
-    //horizontal detection
-    if ((((swipe_det.eX - min_x > swipe_det.sX) || (swipe_det.eX + min_x < swipe_det.sX)) && ((swipe_det.eY < swipe_det.sY + max_y) && (swipe_det.sY > swipe_det.eY - max_y) && (swipe_det.eX > 0)))) {
-      if(swipe_det.eX > swipe_det.sX) direc = "r";
-      else direc = "l";
-    }
-    //vertical detection
-    else if ((((swipe_det.eY - min_y > swipe_det.sY) || (swipe_det.eY + min_y < swipe_det.sY)) && ((swipe_det.eX < swipe_det.sX + max_x) && (swipe_det.sX > swipe_det.eX - max_x) && (swipe_det.eY > 0)))) {
-      if(swipe_det.eY > swipe_det.sY) direc = "d";
-      else direc = "u";
-    }
-
-    if (direc != "") {
-      if(typeof func == 'function') func(el,direc);
-    }
-    direc = "";
-    swipe_det.sX = 0; swipe_det.sY = 0; swipe_det.eX = 0; swipe_det.eY = 0;
-  },false);  
-}
-
-function myfunction(el,d) {
-  alert("you swiped on element with id '"+el+"' to "+d+" direction");
-}
-
-detectswipe('slides',myfunction);
-
-
 
 
 window.onresize = () => {
