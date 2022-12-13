7238032ece6eaeb0c5873165f1eecaf92125c57e
phiddiRegius <bjb462@nyu.edu>
2022-12-13 11:16:35 +0800

    Still a dissapointment

--- circus/coding-exercises/myPractice/p05.0_flowerServer/public/game.js
+++ circus/coding-exercises/myPractice/p05.0_flowerServer/public/game.js
@@ -1,26 +1,50 @@
 // game.js
 
-let mapWidth = 200;
-let mapHeight = 200;
+let mapWidth = 2000;
+let mapHeight = 2000;
 
-// let playerWidth = 10;
-// let playerHeight = 10;
-
-let flowerWidth = 10;
-let flowerHeight = 10;
+let minDist = 50;
 
 let player; 
 
+// let currentLoopIndex = 0;
+// const animationLoop = [1, 2, 3, 4]
+
+// let displayBox = document.createElement('div');
+// displayBox.setAttribute('id', 'displayBox');
+let playButton = document.createElement('button');
+playButton.setAttribute('id', 'playButton');
+playButton.style.backgroundImage = 'url(assets/startButton.png)'
+
+let loader = document.createElement('div');
+loader.setAttribute('id', 'loader');
+loader.style.backgroundImage = 'url(assets/rabillion/rabillion.png)'
+
+let loadScreen = document.createElement('div');
+loadScreen.setAttribute('id', 'loadScreen');
+loadScreen.style.width = mapWidth  + 'px';
+loadScreen.style.height = mapHeight + 'px';
+
 let gameMap = document.createElement('div');
 gameMap.setAttribute('id', 'gameMap');
-document.body.append(gameMap);
 gameMap.style.width = mapWidth  + 'px';
 gameMap.style.height = mapHeight + 'px';
 
+document.body.append(loadScreen);
+loadScreen.append(loader, playButton);
+
+let activePlayers = [];
+let activeObjects = [];
+let activeFlowers = [];
+
 const leftKey = "ArrowLeft";
+let faceLeft = false;
 const rightKey = "ArrowRight";
+let faceRight = false;
 const upKey = "ArrowUp";
+let faceUp = false;
 const downKey = "ArrowDown";
+let faceDown = false;
 
 window.onkeydown = function(event) {
   const keyCode = event.key;
@@ -36,32 +60,52 @@ window.onkeydown = function(event) {
       player.stepDown();
     } 
 }
-  
 
-// Game Objects & Players
+function playGame() {
+  let forestSound = new Audio('assets/sound/forest.mp3');
+  forestSound.play();
+  forestSound.loop = true;
+
+  loadScreen.style.display = "none";
+  document.body.append(gameMap);
+
+  // Game Objects & Players
 class Player {
   constructor(posX, posY, playerId, isMe) {
       // this.posX = 0; // for debugging
       // this.posY = 0; // for debugging
-      this.posX = posX;
-      this.posY = posY;
-      this.width = 10;
-      this.height = 10;
-      this.playerId = playerId;
-      this.speed = 2;
-      this.isMe = isMe;
-      this.elm;
-      this.myFlowers = [];
+      this.posX = posX; // x position
+      this.posY = posY; // y position
+      this.width = 24;
+      this.height = 36;
+      this.playerId = playerId; // socket.id
+      this.speed = 2; // # of px moved
+      this.isMe = isMe; // true or false
+      this.elm; 
+      this.myFlowers = []; // flower inventory
+      this.giftTo;
+      this.reactTrigger;
+      // this.collisionMarker;
+      // this.playerPanel;
+      this.reactTop;
   }
   
   stepRight(){
+    faceRight = true;
     // check if step would collide
     if( this.isColliding(this.posX + this.speed, this.posY) == true ){
 
     }else{
+      this.elm.style.backgroundImage = 'url(assets/rabillion/rabillionRight.png)';
+
       this.posX = this.posX + this.speed;
       socket.emit('playerMovement', ({x: this.posX, y: this.posY})); 
       this.updatePosition();
+
+    
+      // if(this.isCollectingFlower(this.posX, this.posY) == true) {
+      //   // console.log('pickup the flower');
+      // }
     }
     // if not
     // apply step to this.posX 
@@ -69,32 +113,52 @@ class Player {
     // tell server that player moved
   }
   stepLeft() {
+    faceLeft = true;
     if( this.isColliding(this.posX - this.speed, this.posY) == true ){
 
     }else{
+      this.elm.style.backgroundImage = 'url(assets/rabillion/rabillionLeft.png)';
+
       this.posX = this.posX - this.speed;
       socket.emit('playerMovement', ({x: this.posX, y: this.posY})); 
       this.updatePosition();
+
+      // if(this.isCollectingFlower(this.posX, this.posY) == true) {
+      //   // console.log('pickup the flower');
+      // }
     }
 
   }
   stepUp() {
+    faceUp = true;
     if( this.isColliding(this.posX, this.posY - this.speed) == true ){
 
     }else{
+      this.elm.style.backgroundImage = 'url(assets/rabillion/rabillionBack.png)';
       this.posY = this.posY - this.speed;
       socket.emit('playerMovement', ({x: this.posX, y: this.posY})); 
       this.updatePosition();
+
+      // if(this.isCollectingFlower(this.posX, this.posY) == true) {
+      //   // console.log('pickup the flower');
+      // }
     }
 
   }
   stepDown() {
+    faceDown = true;
     if( this.isColliding(this.posX, this.posY + this.speed) == true ){
 
     }else{
+      this.elm.style.backgroundImage = 'url(assets/rabillion/rabillionFront.png)';
       this.posY = this.posY + this.speed;
       socket.emit('playerMovement', ({x: this.posX, y: this.posY})); 
       this.updatePosition();
+
+      // if(this.isCollectingFlower(this.posX, this.posY) == true) {
+      //   // console.log('pickup the flower');
+      // }
+      
     }
 
   }
@@ -112,14 +176,85 @@ class Player {
             (other.posY > nextStepY - other.height) && 
             (other.posY <= nextStepY + this.height) ) {
               return true
+          }
+        }
+        // console.log(activePlayers[i]);      
+      }
+       return false
+    } else {
+      return true
+    }
+  }
+  isCollectingFlower(myPosX, myPosY) { //does not actually use the next step uses where they are
+    if(this.myFlowers.length >= 10) {
+
+    } else {
+      for(let i = 0; i < activeFlowers.length; i++) {
+        let theFlower = activeFlowers[i];
+        if((theFlower.posX > myPosX - theFlower.width) && 
+        (theFlower.posX <= myPosX + this.width) && 
+        (theFlower.posY > myPosY - theFlower.height) && 
+        (theFlower.posY <= myPosY + this.height) ) {
+          // console.log(activeFlowers[i].name);
+  
+          // set isPicked to true
+            activeFlowers[i].isPicked = true;
+            activeFlowers[i].whoPicked = this.playerId;
+            // console.log(this.playerId, ' picked ', activeFlowers[i].id)
+          
+          // emit to server
+            socket.emit('flowerCollected', activeFlowers[i]);
+  
+          // move to players inventory
+          if(this.isMe === true) {
+            this.myFlowers.push(activeFlowers[i]);
+            // console.log(this.myFlowers);
+
+            let gridDisplay = document.querySelector('.myFlowerDisplay');
+           
+            // console.log(childDiv);
+             /// Move to a socket function?
+              for(let i=0; i < this.myFlowers.length; i++) {
+                // console.log(this.myFlowers[i]);
+                //add the image of flower 1 to grid id 1, 2 to 2
+                // let gridNum = 'flwr' + (i + 1);
+                let childDiv = gridDisplay.getElementsByClassName('flwr')[i];
+                childDiv.style.backgroundImage = this.myFlowers[i].flwrIcon;
+              }
+          }
+            
+          
+          return true
+        }
+      } 
+    }
+  }
+  isWithinReach(myPosX, myPosY) {
+  
+    if(this.myFlowers.length < 1) {
+      // console.log("no flowers to give")
+      return false
+    } else {
+      for(let i = 0; i < activePlayers.length; i++) {
+          
+        if(activePlayers[i].playerId != this.playerId) {
+        // other
+        let other = activePlayers[i];
+        if((other.posX > myPosX - other.width - minDist) && 
+            (other.posX <= myPosX + this.width + minDist) && 
+            (other.posY > myPosY - other.height - minDist) && 
+            (other.posY <= myPosY + this.height + minDist) ) {
+
+              // this.nearMe = true;
+              return true
+            } else {
+              this.reactTrigger = false;
+              return false
             }
         }
         // console.log(activePlayers[i]);      
       }
-      return false
-    } else {
-      return true
-    }
+    } 
   }
   updatePosition() {
     // if me:
@@ -130,45 +265,137 @@ class Player {
       this.elm.style.left = this.posX + 'px';
       this.elm.style.top = this.posY  + 'px';
     }
+    if(this.isCollectingFlower(this.posX, this.posY) == true) {
+      // console.log('pickup the flower');
+    }
+    if(this.isWithinReach(this.posX, this.posY) == true) {
+      if(this.reactTrigger == true) {
+        
+      } else {
+        let exclSound = new Audio('assets/sound/exclamation.mp3');
+        exclSound.play();
+
+        this.reactTop.setAttribute('class', 'myReactTop');
+        this.reactTop.style.backgroundImage = 'url(assets/rabillion/reactions/exclamation.png)'
+
+        let otherReact = document.getElementsByClassName('otherReactTop');
+        for(let i=0; i < otherReact.length; i++) {
+          otherReact[i].style.display = 'block';
+          otherReact[i].style.backgroundImage = 'url(assets/rabillion/reactions/gift.png)';
+        }
+
+        this.reactTrigger = true;
+      }
+    } else {
+      this.reactTop.classList.remove('myReactTop');
+
+      let otherReact = document.getElementsByClassName('otherReactTop');
+        for(let i=0; i < otherReact.length; i++) {
+          otherReact[i].style.display = 'none';
+        }
+      // this.reactTop.setAttribute('class', 'reactTop');
+    }
+      // console.log("you can gift a flower");
+      // reaction pops up when you are close enough to give a flower
+        
+     
+      // emit to server
+          // who is sending the flower => this.playerId
+          // what flower => first flower in this.myFlowers?
+          // to who => activePlayers[i].playerId
+          // this.giftTo = activePlayers[i].playerId;
+          // let playerIsGifting = activePlayers.find(player => player.playerId === this.playerId);
+          // console.log(this.playerId, ' wants to gift ', activePlayers[i].playerId, ' a flower');
+          // socket.emit('giftingFlower', playerIsGifting);
+      
+    // } else {
+    //   this.reactTop.classList.remove('reactionExclamation');
+    //   this.reactTop.setAttribute('class', 'reactTop');
+    // }
     // else:
     // move actal this.elm to location
   }
   createElement(){
     this.elm = document.createElement('div');
+    this.collisionMarker = document.createElement('div');
+    this.playerPanel = document.createElement('div');
+    this.reactTop = document.createElement('div');
+    this.flowerDisplay = document.createElement('div');
+
+    for(let i=0; i<10; i++) {
+      this.flwrInventory = document.createElement('div');
+        this.flwrInventory.classList.add('flwr' + (i + 1), 'flwr');
+        // this.flwrInventory.setAttribute('class', 'flwr');
+
+      this.flowerDisplay.append(this.flwrInventory);
+    }
     // if me....
     if(this.isMe === true) {
       this.elm.setAttribute('class', 'mainPlayer');
+      this.collisionMarker.setAttribute('class', 'myMarker');
+      this.playerPanel.setAttribute('class', 'myPanel');
+      this.reactTop.setAttribute('class', 'myReactTop');
+      this.flowerDisplay.setAttribute('class', 'myFlowerDisplay');
     // if not me...
     } else {
       this.elm.setAttribute('class', 'otherPlayer');
+      this.collisionMarker.setAttribute('class', 'otherMarker');
+      this.playerPanel.setAttribute('class', 'otherPanel');
+      this.reactTop.setAttribute('class', 'otherReactTop');
+      this.flowerDisplay.setAttribute('class', 'otherFlowerDisplay');
     }
     // else
     //  ///
-    this.elm.style.width = this.width + 'px';
-    this.elm.style.height = this.height + 'px';
-  
-    // append the mainPlayer to the map
-    gameMap.append(this.elm)
-  
     // assign element id as the socket id
-    this.elm.id =  this.playerId;
-  
-    // push id to activePlayers array 
-    // activePlayers.push(playerInfo.playerId);
-  
+      this.elm.id =  this.playerId;
+      
+      this.elm.style.width = this.width + 'px';
+      this.elm.style.height = this.height + 'px';
+
+      // append the mainPlayer to the map
+      gameMap.append(this.elm)
+
+      this.collisionMarker.style.width = 15 + 'px';
+      this.collisionMarker.style.height = 7 + 'px';
+      this.collisionMarker.style.top = 20 + 'px';
+      this.collisionMarker.style.left = 4.5 + 'px';
+
+      this.elm.append(this.collisionMarker, this.playerPanel);
+
+    // this.elm.append(playerPanel, collisionMarker);
+       this.playerPanel.append(this.reactTop, this.flowerDisplay);
+
+
+    // sprite sheet
+      // let sheet = new Image();
+      // sheet.src = 'assets/rabillion/sheet/walk_back.front.png';
+
+      this.elm.style.backgroundImage = 'url(assets/rabillion/rabillionFront.png)';
+    
+      
     // name tag for debug tracking
-    let nameTag = document.createElement("p");
-    nameTag.innerHTML = this.playerId;
-    this.elm.append(nameTag)
+      // let nameTag = document.createElement("p");
+      // nameTag.innerHTML = this.playerId;
+      // this.elm.append(nameTag)
 
      // set map coordinates from random player location
-    this.updatePosition();
+      this.updatePosition();
+  }
+  myFlowers() {
+    this.elm = document.createElement('div');
+
+  }
+  drawSprite(frameX, frameY) {
+    let spriteX = frameX * this.width;
+    let spriteY = frameY * this.height;
+    this.elm.style.backgroundPosition = `${spriteX}px ${spriteY}px`;
   }
 };
 
 class worldObject {
-    constructor(name, posX, posY, width, height) {
+    constructor(name, id, posX, posY, width, height) {
         this.name = name;
+        this.id = id;
         this.posX = posX;
         this.posY = posY;
         this.width = width;
@@ -177,24 +404,32 @@ class worldObject {
   };
   
   class Flower extends worldObject { 
-    constructor (name, posX, posY, isPicked) {
-        super(name, posX, posY); 
+    constructor (name, id, posX, posY, isPicked, flwrImage, flwrIcon) {
+        super(name, id, posX, posY); 
         // this.scientific = scientific;
         // this.rarity = rarity;
         this.posX = posX;
         this.posY = posY;
-        this.width = 10;
-        this.height = 10;
+        this.width = 14;
+        this.height = 22;
         this.flowerElm;
         this.isPicked = isPicked;
+        this.whoPicked;
+        this.flwrImage = flwrImage;
+        this.flwrIcon = flwrIcon;
     }
     createElement(){
       this.flowerElm = document.createElement('div');
 
       this.flowerElm.setAttribute('class', 'flower');
+      this.flowerElm.setAttribute('id', this.id);
 
       this.flowerElm.style.width = this.width + 'px';
       this.flowerElm.style.height = this.height + 'px';
+
+      this.flwrIcon = 'url(assets/world/blueFlowerIcon.png)'
+      this.flwrImage = 'url(assets/world/blueFlowerPlant.png)';
+      this.flowerElm.style.backgroundImage = this.flwrImage;
 
       gameMap.append(this.flowerElm)
 
@@ -206,15 +441,9 @@ class worldObject {
     }
   };
 
-
   // let flowers = [
   //   ghostOrchid = new Flower ("Ghost Orchid", "Dendrophylax Lindenii", 10, randomPosition() - 5, randomPosition() - 5, 10, 10),
   // ];
-
-
-let activePlayers = [];
-let activeObjects = [];
-let activeFlowers = [];
 
 let socket = io();
  
@@ -224,7 +453,7 @@ socket.on('currentPlayers', function (players) {
     if (players[id].playerId === socket.id) {
       // create an asset for the main player
       if(activePlayers.indexOf(socket.id) === -1) {
-        console.log("adding main player");
+        // console.log("adding main player");
         // addPlayer(players[id]);
         player = new Player (players[id].x, players[id].y, players[id].playerId, true);
         // push player (mainPlayer) instance to activePlayers array
@@ -236,7 +465,7 @@ socket.on('currentPlayers', function (players) {
     } else {
       // otherwise, create an other player
       if(activePlayers.indexOf(socket.id) === -1) {
-        console.log("adding other player");
+        // console.log("adding other player");
         // addOtherPlayers(players[id]);
         let guestPlayer = new Player (players[id].x, players[id].y, players[id].playerId, false);
         // push player (mainPlayer) instance to activePlayers array
@@ -246,6 +475,7 @@ socket.on('currentPlayers', function (players) {
       }
     }
   });
+  // console.log(activePlayers);
 });
 socket.on('newPlayer', function (playerInfo) {
   console.log("A new player has joined");
@@ -267,37 +497,63 @@ socket.on('playerMoved', function (playerInfo) {
   movedPlayer.posY = playerInfo.y;
 
   movedPlayer.updatePosition();
-
-  // let playerOnMove = document.getElementById(playerInfo.playerId)
-  // // let movePlayer = document.getElementById(playerInfo.playerId);
-  // playerOnMove.style.left = playerInfo.x + 'px';
-  // playerOnMove.style.top = playerInfo.y + 'px';
-
-  // console.log(playerInfo.playerId + " moved to: " + playerInfo.x, playerInfo.y);
 });
 socket.on('flowerData', function (flowerData) {
-
-  
   for(let i=0; i < 5; i++) {
-    console.log(flowerData[i].x, flowerData[i].y);
-    let flower = new Flower (flowerData[i].id, flowerData[i].x, flowerData[i].y, flowerData[i].isPicked);
+    // console.log(flowerData[i].x, flowerData[i].y);
+    let flower = new Flower ('a flower', flowerData[i].id, flowerData[i].x, flowerData[i].y, flowerData[i].isPicked);
 
     activeFlowers.push(flower);
     // create mainPlayer element 
     flower.createElement();
   }
-  
-  // gameMap.append(flower);
-  // if collide with flower: emit and push to player's inventory
-  // this.socket.emit('flowerCollected');
-  
 });
 
+socket.on('someonePickedTheFlower', function (playerPicked) {
+  // console.log(playerPicked.whoPicked, ' picked ', playerPicked.id);
+
+   // remove from active flower objects
+   activeFlowers.splice(playerPicked.id, 1);
+  //  console.log(activeFlowers);
+
+   // remove from gameMap
+    // console.log(theFlower.id);
+    let flowerToRemove = document.getElementById(playerPicked.id);
+    gameMap.removeChild(flowerToRemove);
+
+    // if(playerPicked.whoPicked === ) {
+    //   console.log("this is me, do nothing")
+    // } else {
+    //   console.log(playerPicked.whoPicked);
+    //   let thePicker = document.getElementById(playerPicked.whoPicked);
+    //   console.log(thePicker);
+    // }
+    
+
+
+
+    // for(let i=0; i < this.myFlowers.length; i++) {
+    //   // console.log(this.myFlowers[i]);
+    //   //add the image of flower 1 to grid id 1, 2 to 2
+    //   // let gridNum = 'flwr' + (i + 1);
+    //   let childDiv = gridDisplay.getElementsByClassName('flwr')[i];
+    //   childDiv.style.backgroundImage = this.myFlowers[i].flwrIcon;
+    // }
+
+});
+
+socket.on('newFlower', function (newFlower) {
+  // console.log(newFlower.x, newFlower.y);
+  let flower = new Flower ('a flower', newFlower.id, newFlower.x, newFlower.y, newFlower.isPicked);
+  activeFlowers.push(flower);
+  flower.createElement();
+  console.log('a new flower is growing!');
+});
 
 socket.on('disconnectUser', function (playerId) {
     // remove the div element of the disconnected player
     let el = document.getElementById(playerId);
-    console.log(el);
+    // console.log(el);
     el.remove(gameMap);
 
     console.log(activePlayers);
@@ -309,6 +565,17 @@ socket.on('disconnectUser', function (playerId) {
 
     console.log("A player has left the game");
 });
+
+}
+
+playButton.addEventListener("click", playGame)
+
+
+
+
+
+
+
 
 
 
--- circus/coding-exercises/myPractice/p05.0_flowerServer/server.js
+++ circus/coding-exercises/myPractice/p05.0_flowerServer/server.js
@@ -13,43 +13,32 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
+let minDist = 10;
+
+let coordX;
+let coordY;
+
 function randomPosition() {
-  let random = parseInt( (50 + Math.random()*100));
+  let random = parseInt( (50 + Math.random()*1800) );
+
   return random
 }
  
-let flower = [
-  {
-    id: "flowerOne",
-    x: randomPosition() - 5,
-    y: randomPosition() -5,
-    isPicked: false
-  },
-  {
-    id: "flowerTwo",
-    x: randomPosition() - 5,
-    y: randomPosition() -5,
-    isPicked: false
-  },
-  {
-    id: "flowerThree",
-    x: randomPosition() - 5,
-    y: randomPosition() -5,
-    isPicked: false
-  },
-  {
-    id: "flowerFour",
-    x: randomPosition() - 5,
-    y: randomPosition() -5,
-    isPicked: false
-  },
-  {
-    id: "flowerFive",
-    x: randomPosition() - 5,
-    y: randomPosition() -5,
-    isPicked: false
+let flowers = []
+
+
+for(let i=0; i < 5; i++) {
+  let flower = {
+    id: "flower" + (i + 1),
+    x: randomPosition() - 14,
+    y: randomPosition() - 22,
+    width: 14,
+    height: 22,
+    isPicked: false,
   }
-]
+  flowers.push(flower);
+}
+
 players = {};
 
 
@@ -60,29 +49,56 @@ players = {};
 io.on('connection', function (socket) {
   console.log('a user connected: ', socket.id);
 
-    // create a new player and add it to our players object
+  // create a new player and add it to our players object
   players[socket.id] = {
     // x and y positioning of the map:
     // subtract half of the playerWidth and playerHeight 
-    x: randomPosition() - 5,
-    y: randomPosition() - 5,
+    x: randomPosition() - 24,
+    y: randomPosition() - 36,
     // x: x, // for debugging
     // y: y, // for debugging
+    width: 24,
+    height: 36,
     playerId: socket.id,
+    myFlowers: [],
   };
   // x += 50; // for debugging
   // if(x>200) x=10; // for debugging
 
-  console.log(players);
+  // console.log(players);
 
+  // function checkCoordinates() {
+  //   let coordX = randomPosition();
+  //   let coordY = randomPosition();
+
+  //   console.log('new positions: ', coordX, coordY);
+
+  //   for(const player in players) {
+  //     console.log(player, players[player].x, players[player].y)
+  //     if((coordX > players[player].x - players[player].width - minDist) &&
+  //     (coordX <= players[player].x + players[player].width + minDist) &&
+  //     (coordY > players[player].y - players[player].width - minDist) &&
+  //     (coordY <= players[player].y + players[player].width + minDist)) {
+  //       // console.log('cannot place player here');
+  //       return true
+  //     } 
+  //   }
+  // }
+
+  
+  // players[socket.id].x = coordX;
+  // players[socket.id].y = coordY;
+  socket.broadcast.emit('newPlayer', players[socket.id]);
+  // console.log(players[socket.id]);
+ 
   // send the players object to the new player
   socket.emit('currentPlayers', players);
-  // update all other players of the new player
-  socket.broadcast.emit('newPlayer', players[socket.id]);
-  // send flowers to the new player
-  socket.emit('flowerData', flower);
-  console.log(flower);
 
+  // send the flower object to the new player
+  socket.emit('flowerData', flowers);
+  
+ 
+  
   // when a player moves, update the player data
   socket.on('playerMovement', function (movementData) {
     // console.log(movementData);
@@ -91,12 +107,47 @@ io.on('connection', function (socket) {
     // console.log(movementData.x, movementData.y)
     // emit a message to all players about the player that moved
     socket.broadcast.emit('playerMoved', players[socket.id]);
+    // console.log(players);
   });
+
+  socket.on('flowerCollected', function (flowerIsPicked) {
+    // console.log(flowerIsPicked);
+
+  socket.emit('someonePickedTheFlower', flowerIsPicked);
+
+    // generate new random position to replace collected flower
+    let flower = {
+      id: flowerIsPicked.id,
+      x: randomPosition() - 14,
+      y: randomPosition() - 22,
+      width: 14,
+      height: 22,
+      isPicked: false,
+    }
+
+    // remove the picked flower from the flower arrat
+    let flowerToRemove = flowers.find(flower => flower.id === flowerIsPicked.id);
+    flowers.splice(flowerToRemove, 1);
+
+    // push new flower to the array
+    flowers.push(flower);
+    
+    // new flower to players
+    io.emit('newFlower', flower);
+  });
+
+  socket.on('giftingFlower', function (playerInfo) {
+    console.log(playerInfo.playerId, ' gifted ', playerInfo.giftTo)
+  });
+
+  // update the current flowers arrays
+  // socket.emit('currentFlowers', flowers);
 
   // when a player disconnects, remove them from our players object
   socket.on('disconnect', function () {
     console.log('user disconnected: ', socket.id);
     delete players[socket.id];
+    delete flowers;
     // emit a message to all players to remove this player
     io.emit('disconnectUser', socket.id);
   });
